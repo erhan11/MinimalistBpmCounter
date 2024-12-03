72f0e5b115800056f02e8dce6c9d114297e004e1
erhan11 <erhan0@hotmail.com>
2024-12-03 14:38:38 +0200

    Initial commit

--- backend/routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (error) {
-    res.status(500).send({error: 'Failed to fetch users'});
-  }
-});
